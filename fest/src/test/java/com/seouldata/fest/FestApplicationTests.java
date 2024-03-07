7c7bd2ee615fc6fe1a71ac0274600f47f75cb90a
jieun21124 <jieun21124@naver.com>
2024-03-07 16:13:58 +0900

    init: fest 프로젝트 생성

---
+++ fest/src/test/java/com/seouldata/fest/FestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seouldata.fest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
