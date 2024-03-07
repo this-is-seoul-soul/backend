db0f4020f53a7986c832fd851d57ef862ad2ec23
jieun21124 <jieun21124@naver.com>
2024-03-07 16:13:58 +0900

    init: fest 프로젝트 생성

---
+++ fest/src/main/java/com/seouldata/fest/FestApplication.java
@@ -0,0 +1,13 @@
+package com.seouldata.fest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestApplication.class, args);
+	}
+
+}
