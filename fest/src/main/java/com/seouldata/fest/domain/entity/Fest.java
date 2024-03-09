fe3dbc87a37d4d94de6f6016ab0c872fdfd60be1
jini_11 <78905126+jini11@users.noreply.github.com>
2024-03-09 19:29:13 +0900

    SEOULDATA-16 [FEATURE] 축제 Entity 생성 (#1)
    * feat/SEOULDATA-16 chore: db 연결 관련 설정 추가
    
    * feat/SEOULDATA-16 feat: fest entity 생성
    
    * feat/SEOULDATA-16 feat: fest repository 생성
    
    * feat/SEOULDATA-16 test: insert fest dummy 구현

---
+++ fest/src/main/java/com/seouldata/fest/domain/entity/Fest.java
@@ -0,0 +1,93 @@
+package com.seouldata.fest.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "fest")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Fest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long festSeq;
+
+    @Column(length = 100)
+    @NotNull
+    private String title;
+
+    @Column
+    @NotNull
+    private int codename;
+
+    @Transient
+    @Enumerated(EnumType.STRING)
+    private Codename codeType;
+
+    @Column(length = 4)
+    @NotNull
+    private String guname;
+
+    @Column(length = 160)
+    @NotNull
+    private String place;
+
+    @Column(name = "use_trgt", length = 90)
+    @NotNull
+    private String useTrgt;
+
+    @Column(name = "is_free", length = 2)
+    private char isFree;
+
+    @Column(name = "use_fee", length = 270)
+    @NotNull
+    private String useFee;
+
+    @Column(name = "start_date")
+    @NotNull
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    @NotNull
+    private LocalDateTime endDate;
+
+    @Column
+    private double lot;
+
+    @Column
+    private double lat;
+
+    @Column(name = "org_link", length = 550)
+    private String orgLink;
+
+    @Column(name = "main_img", length = 120)
+    private String mainImg;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "tinyint(1)")
+    @ColumnDefault("0")
+    private boolean isDeleted;
+
+    @Builder
+    public Fest(String title, int codename, String guname, String place, String useTrgt, char isFree, String useFee, LocalDateTime startDate, LocalDateTime endDate, double lot, double lat, String orgLink, String mainImg, boolean isDeleted) {
+        this.title = title;
+        this.codename = codename;
+        this.guname = guname;
+        this.place = place;
+        this.useTrgt = useTrgt;
+        this.isFree = isFree;
+        this.useFee = useFee;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.lot = lot;
+        this.lat = lat;
+        this.orgLink = orgLink;
+        this.mainImg = mainImg;
+        this.isDeleted = isDeleted;
+    }
+
+}
