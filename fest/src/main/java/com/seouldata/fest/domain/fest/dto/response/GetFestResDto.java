6edfc0f3c4404b36c6563942052c877e8a84527f
So_ob <dhrtnqls0535@naver.com>
2024-03-11 02:25:11 +0900

    feat/SEOULDATA-51 feat: 축제 공공데이터 ResponseDto 생성

---
+++ fest/src/main/java/com/seouldata/fest/domain/fest/dto/response/GetFestResDto.java
@@ -0,0 +1,73 @@
+package com.seouldata.fest.domain.fest.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class GetFestResDto {
+
+    private CulturalEventInfo culturalEventInfo;
+
+    @Data
+    @RequiredArgsConstructor
+    public static class CulturalEventInfo {
+
+        @JsonProperty("list_total_count")
+        private int listTotalCount;
+
+        private List<Row> row;
+
+    }
+
+    @Data
+    public static class Row {
+
+        @JsonProperty("CODENAME")
+        private String codename;
+
+        @JsonProperty("GUNAME")
+        private String guname;
+
+        @JsonProperty("TITLE")
+        private String title;
+
+        @JsonProperty("ORG_NAME")
+        private String orgName;
+
+        @JsonProperty("USE_TRGT")
+        private String useTrgt;
+
+        @JsonProperty("USE_FEE")
+        private String useFee;
+
+        @JsonProperty("ORG_LINK")
+        private String orgLink;
+
+        @JsonProperty("MAIN_IMG")
+        private String mainImg;
+
+        @JsonProperty("STRTDATE")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.S")
+        private LocalDateTime startDate;
+
+        @JsonProperty("END_DATE")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.S")
+        private LocalDateTime endDate;
+
+        @JsonProperty("LOT")
+        private double lot;
+
+        @JsonProperty("LAT")
+        private double lat;
+
+        @JsonProperty("IS_FREE")
+        private String IS_FREE;
+
+    }
+
+}
