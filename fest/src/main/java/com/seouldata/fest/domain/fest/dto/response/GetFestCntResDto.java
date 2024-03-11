c386343892caf2c4cb108b4058bb27c3d89f9ee4
So_ob <dhrtnqls0535@naver.com>
2024-03-11 17:47:35 +0900

    feat/SEOULDATA-51 feat: 행사 공공데이터 개수 Response Dto 생성

---
+++ fest/src/main/java/com/seouldata/fest/domain/fest/dto/response/GetFestCntResDto.java
@@ -0,0 +1,21 @@
+package com.seouldata.fest.domain.fest.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class GetFestCntResDto {
+
+    private GetFestResDto.CulturalEventInfo culturalEventInfo;
+
+    @Data
+    @RequiredArgsConstructor
+    public static class CulturalEventInfo {
+
+        @JsonProperty("list_total_count")
+        private int listTotalCount;
+
+    }
+
+}
