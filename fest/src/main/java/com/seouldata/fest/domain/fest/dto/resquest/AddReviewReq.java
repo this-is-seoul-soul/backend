13ed8bebc901e8d3537fa795323b8bfa8897e9f6
jini_11 <jieun21124@naver.com>
2024-03-19 18:33:16 +0900

    feat/SEOULDATA-65 feat: 행사 리뷰 등록 API 구현

---
+++ fest/src/main/java/com/seouldata/fest/domain/fest/dto/resquest/AddReviewReq.java
@@ -0,0 +1,32 @@
+package com.seouldata.fest.domain.fest.dto.resquest;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class AddReviewReq {
+
+    @NotBlank
+    @Length(max = 150)
+    private String content;
+
+    @NotNull
+    @Min(0)
+    @Max(5)
+    private int point;
+
+    private List<String> imgUrl;
+
+    private List<Integer> tag;
+
+    @NotNull
+    private Long festSeq;
+
+}
--- fest/src/main/java/com/seouldata/fest/domain/fest/repository/FestRepository.java
+++ fest/src/main/java/com/seouldata/fest/domain/fest/repository/FestRepository.java
@@ -4,9 +4,13 @@ import com.seouldata.fest.domain.fest.entity.Fest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FestRepository extends JpaRepository<Fest, Long> {
 
     int countAllByIsPublic(boolean isPublic);
+
+    Optional<Fest> findByFestSeq(Long festSeq);
 
 }
