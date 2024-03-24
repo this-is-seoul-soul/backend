2d71dde5194008bc40c3ced8613664efac8d9d97
Park Yehan <parkyehan2060@gmail.com>
2024-03-25 00:41:53 +0900

    feat/SEOULDATA-20 chore: JPA configuration 정의

---
+++ auth/src/main/java/com/seouldata/auth/global/database/JpaAuditorAware.java
@@ -0,0 +1,22 @@
+package com.seouldata.auth.global.database;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JpaAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.of(authentication.getPrincipal().toString());
+    }
+}
