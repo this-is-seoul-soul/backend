ee990b8253977848ecc52918b9ff84571d872140
jini_11 <78905126+jini11@users.noreply.github.com>
2024-03-27 15:25:28 +0900

    SEOULDATA-66 [FEATURE] 행사 리뷰 수정 API 구현 (#12)
    * feat/SEOULDATA-66 fix: ReviewController 테스트 관련 버그 수정
    
    * feat/SEOULDATA-66 rename: ReviewController 위치 수정
    
    * feat/SEOULDATA-66 feat: 행사 리뷰 수정 API 구현
    
    * feat/SEOULDATA-66 fix: 행사 리뷰 등록 로직 추가

--- fest/src/main/java/com/seouldata/fest/domain/review/repository/ReviewRepository.java
+++ fest/src/main/java/com/seouldata/fest/domain/review/repository/ReviewRepository.java
@@ -4,6 +4,11 @@ import com.seouldata.fest.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<Review> findByReviewSeq(Long reviewSeq);
+
 }
