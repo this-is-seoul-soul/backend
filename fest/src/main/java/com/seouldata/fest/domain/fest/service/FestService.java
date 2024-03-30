135c85637d32c9d169ff009fa3282668343fb86b
So_ob <dhrtnqls0535@naver.com>
2024-03-30 14:28:35 +0900

    feat/SEOULDATA-42 [FEATURE] 행사 등록 API 구현 (#13)
    * feat/SEOULDATA-42 fix: 중복된 ReviewController.java 파일 삭제
    
    * feat/SEOULDATA-42 refactor: Fest Entity 수정
    creator(작성자의 member_seq) 컬럼 추가
    공공데이터의 경우 creator = 0
    
    * feat/SEOULDATA-42 feat: 행사 등록 API 구현
    
    * feat/SEOULDATA-42 fix: 행사 등록자 타입 수정
    fest 테이블 creator 컬럼 int -> Long

--- fest/src/main/java/com/seouldata/fest/domain/fest/service/FestService.java
+++ fest/src/main/java/com/seouldata/fest/domain/fest/service/FestService.java
@@ -1,7 +1,11 @@
 package com.seouldata.fest.domain.fest.service;
 
+import com.seouldata.fest.domain.fest.dto.request.AddFestReq;
+
 public interface FestService {
 
     void getFestData();
+
+    void addFest(Long memSeq, AddFestReq addFestReq);
 
 }
