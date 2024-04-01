b38454c978edcd9e16a9139af3457f5fcb21d48e
jini_11 <jieun21124@naver.com>
2024-04-01 23:30:54 +0900

    feat/SEOULDATA-48 feat: 행사 찜 추가, 삭제 기능 구현

---
+++ fest/src/main/java/com/seouldata/fest/domain/heart/controller/HeartController.java
@@ -0,0 +1,35 @@
+package com.seouldata.fest.domain.heart.controller;
+
+import com.seouldata.common.response.EnvelopResponse;
+import com.seouldata.fest.domain.heart.service.HeartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fest/heart")
+public class HeartController {
+
+    private final HeartService heartService;
+
+    @PostMapping("/{festSeq}")
+    public ResponseEntity<EnvelopResponse> addHeart(@RequestHeader("memSeq") Long memSeq, @PathVariable("festSeq") Long festSeq) {
+
+        heartService.addHeart(memSeq, festSeq);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(EnvelopResponse.builder().code(HttpStatus.CREATED.value()).build());
+    }
+
+    @DeleteMapping("/{heartSeq}")
+    public ResponseEntity<EnvelopResponse> removeHeart(@RequestHeader("memSeq") Long memSeq, @PathVariable("heartSeq") Long heartSeq) {
+
+        heartService.removeHeart(heartSeq);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(EnvelopResponse.builder().code(HttpStatus.OK.value()).build());
+    }
+
+}
--- fest/src/main/java/com/seouldata/fest/domain/heart/repository/HeartRepository.java
+++ fest/src/main/java/com/seouldata/fest/domain/heart/repository/HeartRepository.java
@@ -4,6 +4,11 @@ import com.seouldata.fest.domain.heart.entity.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+
+    Optional<Heart> findByHeartSeq(Long heartSeq);
+
 }
