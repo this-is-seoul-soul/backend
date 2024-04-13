a909e307af319681ca7a1321796513f938b51f7a
Park Yehan <parkyehan2060@gmail.com>
2024-04-13 17:51:42 +0900

    feat/SEOULDATA-36 feat: 회원가입 API 구현

---
+++ auth/src/main/java/com/seouldata/auth/domain/auth/repository/AuthRepository.java
@@ -0,0 +1,14 @@
+package com.seouldata.auth.domain.auth.repository;
+
+import com.seouldata.auth.domain.auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthRepository extends JpaRepository<Member, Long> {
+
+    Optional<Object> findByEmail(String email);
+
+    Optional<Object> findByNickname(String nickname);
+
+}
