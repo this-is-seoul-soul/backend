074bdf09efba28fb19df04d0673d5d2d6d5633b3
Park Yehan <parkyehan2060@gmail.com>
2024-04-21 20:46:36 +0900

    feat/SEOULDATA-98 feat: Badge가 없는 경우 예외 처리

--- auth/src/main/java/com/seouldata/auth/global/exception/AuthErrorCode.java
+++ auth/src/main/java/com/seouldata/auth/global/exception/AuthErrorCode.java
@@ -12,6 +12,8 @@ public enum AuthErrorCode implements ErrorCode {
     USER_ALREADY_EXISTS(500, "이미 가입된 사용자입니다."),
 
     NICKNAME_ALREADY_EXISTS(500, "이미 사용중인 닉네임입니다."),
+
+    BADGE_NOT_FOUND(500, "뱃지를 찾을 수 없습니다.")
     ;
 
     private final int code;
