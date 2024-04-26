c8c2a9b7f7edc5155870830985712601de381bd8
So_ob <dhrtnqls0535@naver.com>
2024-04-26 12:42:39 +0900

    SEOULDATA-123 [FEATURE] 행사 추천 API 조회 (#47)
    - 멤버와 일치하는 MBTI 3개 이상인 행사 최신 순으로 10개 조회

--- fest/src/main/java/com/seouldata/fest/domain/fest/entity/Codename.java
+++ fest/src/main/java/com/seouldata/fest/domain/fest/entity/Codename.java
@@ -4,30 +4,35 @@ import lombok.Getter;
 
 public enum Codename {
 
-    MUSICAL_OPERA("뮤지컬/오페라", 1),
-    CLASSIC("클래식", 2),
-    TRADITION_MUSIC("국악", 3),
-    THEATER("연극", 4),
-    DANCING("무용", 5),
-    EXHIBITION_ART("전시/미술", 6),
-    EDUCATION_EXPERIENCE("교육/체험", 7),
-    CONCERT("콘서트", 8),
-    SOLO_RECITAL("독주/독창회", 9),
-    MOVIE("영화", 10),
-    ETC("기타", 11),
-    FEST_CIVIL("축제-시민화합", 12),
-    FEST_TRADITION_HISTORY("축제-전통/역사", 13),
-    FEST_CULTURE_ART("축제-문화/예술", 14),
-    FEST_ETC("축제-기타", 15),
-    FEST_NATURE_LANDSCAPE("축제-자연/경관", 16);
+    MUSICAL_OPERA("뮤지컬/오페라", 1, "SACE"),
+    CLASSIC("클래식", 2, "SACI"),
+    TRADITION_MUSIC("국악", 3, "SACE"),
+    THEATER("연극", 4, "SVCE"),
+    DANCING("무용", 5, "SVCE"),
+    EXHIBITION_ART("전시/미술", 6, "SVCI"),
+    EDUCATION_EXPERIENCE("교육/체험", 7, "DVTE"),
+    CONCERT("콘서트", 8, "SACE"),
+    SOLO_RECITAL("독주/독창회", 9, "SATE"),
+    MOVIE("영화", 10, "SVCI"),
+    ETC("기타", 11, "XXXX"),
+    FEST_CIVIL("축제-시민화합", 12, "DVCE"),
+    FEST_TRADITION_HISTORY("축제-전통/역사", 13, "DVCE"),
+    FEST_CULTURE_ART("축제-문화/예술", 14, "DVCE"),
+    FEST_ETC("축제-기타", 15, "XXXX"),
+    FEST_NATURE_LANDSCAPE("축제-자연/경관", 16, "DVCE");
+
 
     @Getter
     private final String codeType;
+    @Getter
     private final int codeNum;
+    @Getter
+    private final String festi;
 
-    Codename(String codeType, int codeNum) {
+    Codename(String codeType, int codeNum, String festi) {
         this.codeType = codeType;
         this.codeNum = codeNum;
+        this.festi = festi;
     }
 
     public static int getCodeNum(String codeType) {
