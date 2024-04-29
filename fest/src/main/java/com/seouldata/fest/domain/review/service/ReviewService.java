c22723d43e09e2f6e2be2a7cb373da891f6f2b42
jini_11 <78905126+jini11@users.noreply.github.com>
2024-04-29 17:36:42 +0900

    SEOULDATA-107 [FIX] 축제 리뷰 관련 API 수정 (#50)
    * feat/SEOULDATA-107 fix: 축제 상세 조회 api res 수정
    
    * feat/SEOULDATA-107 feat: 리뷰 태그 조회 API 분리

--- fest/src/main/java/com/seouldata/fest/domain/review/service/ReviewService.java
+++ fest/src/main/java/com/seouldata/fest/domain/review/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.seouldata.fest.domain.review.service;
 import com.seouldata.fest.domain.review.dto.request.AddReviewReq;
 import com.seouldata.fest.domain.review.dto.request.ModifyReviewReq;
 import com.seouldata.fest.domain.review.dto.response.GetReviewTotalRes;
+import com.seouldata.fest.domain.review.dto.response.GetTagRes;
 
 public interface ReviewService {
 
@@ -13,5 +14,7 @@ public interface ReviewService {
     void modifyReview(Long memSeq, ModifyReviewReq modifyReviewReq);
 
     void removeReview(Long memSeq, Long reviewSeq);
+
+    GetTagRes findTag(Long memSeq, Long festSeq);
 
 }
